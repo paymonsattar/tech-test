7a2adc63724aa8b0ef27eed7ac98e295dce38aaf
Paymon Sattarzadeh <47405092+paymonsattar@users.noreply.github.com>
2022-08-14 13:57:08 +0100

    Highlight biggest elem not just biggest section

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ overlayTextElem.appendChild(TextElemNode)
 let startingCoords = {}
 let endingCoords = {}
 let selecting = false
+let elemsInArea = []
 
 selectionElem.style.boxShadow = '0px 0px 0px 9999px rgba(0,0,0,.6)'
 selectionElem.style.content = ''
@@ -43,49 +44,51 @@ window.onload=function(){
   document.body.onmouseup = function(e) {
     selecting = false
 
-    getLargestElemInArea (startingCoords.x, endingCoords.x, startingCoords.y, endingCoords.y)
-    
-    // Remove after half a second, instant removal looks glitchy.
-    setTimeout(() => {
-      selectionElem.remove()
-      overlayTextElem.remove()
-    }, 500)
+    getLargestElemInArea(startingCoords.x, endingCoords.x, startingCoords.y, endingCoords.y)
+
+    addBorderToLargestElem()
+
+    selectionElem.remove()
+    overlayTextElem.remove()
     
   }
 }
 
 function getLargestElemInArea() {
-  console.log('startX', startingCoords.x)
-  console.log('endX', endingCoords.x)
-  console.log('startingCoords.Y', startingCoords.y)
-  console.log('endingCoords.y', endingCoords.y)
   for (let x = startingCoords.x; x <= endingCoords.x; x = x + 10) {
     for (let y = startingCoords.y; y <= endingCoords.y; y = y + 10) {
       let elemsFromPoint = document.elementsFromPoint(x, y)
-      let section = elemsFromPoint.find(el => el.nodeName === 'SECTION')
-      console.log('section')
-      if (section && isInArea(section)) {
-        console.log('isInArea', isInArea(section))
-        section.classList.add('border-2', 'border-red-400')
-      }
+
+      elemsFromPoint.forEach(el => {
+        if(isInArea(el) && !elemListIncludes(el)) {
+          elemsInArea.push(el)
+        }
+      })
     }
   }
-  let test = document.elementsFromPoint(startingCoords.x, startingCoords.y)
-
-  console.log('test', test)
 }
 
 function isInArea(element) {
   let position = element.getBoundingClientRect();
 
-  console.log('position.x >= startingCoords.x && position.y >= startingCoords.y ', position.x >= startingCoords.x && position.y >= startingCoords.y )
-  console.log('position.x + position.width <= endingCoords.x', position.x + position.width <= endingCoords.x)
-  console.log('position.y + position.height <= endingCoords.y', position.y + position.height <= endingCoords.y)
-  if (position.x >= startingCoords.x && position.y >= startingCoords.y 
+  return position.x >= startingCoords.x && position.y >= startingCoords.y 
     && position.x + position.width <= endingCoords.x
-    && position.y + position.height <= endingCoords.y) {
-      return true
-    }
-    console.log('position', position)
-    return false
+    && position.y + position.height <= endingCoords.y
+}
+
+function elemListIncludes(el) {
+  return elemsInArea.filter(e => e.offsetHeight === el.offsetHeight && e.offsetWidth === el.offsetWidth).length > 0
+}
+
+function addBorderToLargestElem() {
+  let sortedElemsInArea = elemsInArea.sort((a, b) => {
+    return b.offsetHeight - a.offsetHeight || b.offsetWidth - a.offsetWidth;
+  })
+
+  if (sortedElemsInArea.length > 0) {
+    sortedElemsInArea[0].classList.add('border-2', 'border-red-400')
+
+    //tailwind isn't overrriding some borders
+    sortedElemsInArea[0].style.borderColor = 'rgb(248 113 113)'
+  }
 }
