462d4cfff25cffd1a8ced70001ec270973e8b769
Paymon Sattarzadeh <47405092+paymonsattar@users.noreply.github.com>
2022-08-19 20:14:16 +0100

    Load overlay on shift+q key

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
-const selectionElem = document.createElement('div');
-const overlayTextElem = document.createElement('p');
-const TextElemNode = document.createTextNode('Click and drag to select on the page')
-
-overlayTextElem.classList.add('fixed', 'top-0', 'left-0', 'bg-gray-600', 'w-72', 'm-4', 'p-2', 'text-center', 'text-white', 'opacity-90', 'text-opacity-100', 'border-2', 'border-black', 'z-20')
-selectionElem.classList.add('absolute', 'border-1', 'z-10', 'select-none', 'pointer-events-none')
-
-overlayTextElem.appendChild(TextElemNode)
-
+let selectionElem
+let overlayTextElem
+let TextElemNode
 let startingCoords = {}
 let endingCoords = {}
 let selecting = false
 let elemsInArea = []
 
-selectionElem.style.boxShadow = '0px 0px 0px 9999px rgba(0,0,0,.6)'
-selectionElem.style.content = ''
-
 window.onload=function(){
-  document.body.classList.add('select-none')
-  
-  document.body.appendChild(selectionElem)
-  document.body.appendChild(overlayTextElem)
+  displayOverlay()
 
   document.body.onmousedown = function(e) {
     selecting = true
@@ -54,6 +42,34 @@ window.onload=function(){
   }
 }
 
+window.onkeypress = function(event) {
+  console.log(event.keyCode)
+  if (event.keyCode === 81) {
+     displayOverlay()
+  }
+}
+
+function displayOverlay() {
+  removeBorders()
+
+  selectionElem = document.createElement('div');
+  overlayTextElem = document.createElement('p');
+  TextElemNode = document.createTextNode('Click and drag to select on the page')
+
+  overlayTextElem.classList.add('fixed', 'top-0', 'left-0', 'bg-gray-600', 'w-72', 'm-4', 'p-2', 'text-center', 'text-white', 'opacity-90', 'text-opacity-100', 'border-2', 'border-black', 'z-20')
+  selectionElem.classList.add('absolute', 'border-1', 'z-10', 'select-none', 'pointer-events-none')
+
+  overlayTextElem.appendChild(TextElemNode)
+
+  selectionElem.style.boxShadow = '0px 0px 0px 9999px rgba(0,0,0,.6)'
+  selectionElem.style.content = ''
+
+  document.body.classList.add('select-none')
+  
+  document.body.appendChild(selectionElem)
+  document.body.appendChild(overlayTextElem)
+}
+
 function getLargestElemInArea() {
   for (let x = startingCoords.x; x <= endingCoords.x; x = x + 10) {
     for (let y = startingCoords.y; y <= endingCoords.y; y = y + 10) {
@@ -80,10 +96,14 @@ function elemListIncludes(el) {
   return elemsInArea.filter(e => e.offsetHeight === el.offsetHeight && e.offsetWidth === el.offsetWidth).length > 0
 }
 
-function addBorderToLargestElem() {
-  let sortedElemsInArea = elemsInArea.sort((a, b) => {
+function getSortedElems() {
+  return elemsInArea.sort((a, b) => {
     return b.offsetHeight - a.offsetHeight || b.offsetWidth - a.offsetWidth;
   })
+}
+
+function addBorderToLargestElem() {
+  let sortedElemsInArea = getSortedElems()
 
   if (sortedElemsInArea.length > 0) {
     sortedElemsInArea[0].classList.add('border-2', 'border-red-400')
@@ -91,4 +111,18 @@ function addBorderToLargestElem() {
     //tailwind isn't overrriding some borders
     sortedElemsInArea[0].style.borderColor = 'rgb(248 113 113)'
   }
+}
+
+function removeBorders() {
+  let sortedElemsInArea = getSortedElems()
+
+  if (sortedElemsInArea.length > 0) {
+    sortedElemsInArea[0].classList.remove('border-2', 'border-red-400')
+    sortedElemsInArea[0].style.borderColor = ''
+  }
+  
+  startingCoords = {}
+  endingCoords = {}
+  selecting = false
+  elemsInArea = []
 }
